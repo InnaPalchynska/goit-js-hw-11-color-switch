299f88d8a66eadf716ade689773313db1b4b50c5
Inna Palchynska <InnaPalchynska@gmail.com>
2021-05-28 12:37:28 +0300

    done

---
+++ src/index.js
@@ -0,0 +1,37 @@
+const SET_COLOR_DELAY = 1000;
+let setColorInterval;
+
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+const refs = {
+  startBtn: document.querySelector('button[data-action="start"]'),
+  stopBtn: document.querySelector('button[data-action="stop"]'),
+};
+
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick() {
+  refs.startBtn.disabled = true;
+  setColorInterval = setInterval(setColor, SET_COLOR_DELAY);
+}
+
+function onStopBtnClick() {
+  refs.startBtn.disabled = false;
+  clearInterval(setColorInterval);
+}
+
+function setColor() {
+  document.body.style.backgroundColor = colors[randomIntegerFromInterval(0, 5)];
+}
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
